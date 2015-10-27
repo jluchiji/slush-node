4c5c4f0b974e21fdf13853b303507640164c772f
Denis Luchkin-Zhou <denis@ricepo.com>
2015-10-27 14:32:14 -0400

    Fixing some issues with testing setup

--- templates/test/index.spec.js
+++ templates/test/index.spec.js
@@ -1,6 +1,21 @@
 /**
- * index.js
+ * test/index.spec.js
  *
  * @author  <%= authorName %> <<%= authorEmail %>>
  * @license <%= license %>
+ */
+
+const Chai         = require('chai');
+Chai.use(require('sinon-chai'));
+Chai.use(require('chai-as-promised'));
+
+/*!
+ * Setup global stuff here.
+ */
+global.co          = require('bluebird').coroutine;
+global.expect      = Chai.expect;
+global.Sinon       = require('sinon');
+
+/*!
+ * Start tests.
  */

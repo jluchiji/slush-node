bf2385cd754dea442a848cf2c8da39d350fea201
Denis Luchkin-Zhou <denis@ricepo.com>
2016-01-27 18:34:40 -0500

    Improved toolkit for testing

--- templates/test/index.spec.js
+++ templates/test/index.spec.js
@@ -7,12 +7,17 @@
 
 const Chai         = require('chai');
 Chai.use(require('sinon-chai'));
+Chai.use(require('chai-properties'));
 Chai.use(require('chai-as-promised'));
+
+const Path         = require('path');
+const Root         = require('app-root-path');
+Root.setPath(Path.resolve(__dirname, '../src'));
 
 /*!
  * Setup global stuff here.
  */
-global.co          = require('bluebird').coroutine;
+global.dofile      = Root.require;
 global.expect      = Chai.expect;
 global.Sinon       = require('sinon');
 
